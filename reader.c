5aa8b8ee7324a8a43ebe39dc73970c8f066bc67d
danielbeale-ITguy <danielbeale719@gmail.com>
2025-12-01 20:33:34 +1100

    Update reader.c
    Change it to take input from Scanf function for different files.

--- reader.c
+++ reader.c
@@ -5,13 +5,18 @@ int main(){
 
         FILE *fptr;
 
-        fptr = fopen("tt.txt", "r");
+        char filename[30];
 
-        char String[1000];
-                
-        while(fgets(String, 1000, fptr)){
+        char String[100];
+
+        printf("Please input a Filename>\t\n");
+
+        scanf("%s",filename);
+
+        fptr = fopen(filename, "r");
+
+        while(fgets(String, 100, fptr)){
                 printf("%s", String);
                 }
 
-                
 }
